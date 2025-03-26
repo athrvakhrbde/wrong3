bdc0ed380d16654450f49350a31b5d1e6e6b49eb
Atharva Kharbade <atharvakharbade@gmail.com>
2025-03-26 23:15:01 +0530

    Complete overhaul for Netlify compatibility

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  target: 'serverless',
+  webpack: (config, { isServer }) => {
+    // Fix npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 }
