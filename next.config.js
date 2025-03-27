4062572068e2ffc2fcf3c389238358e8d9bd3e6d
Atharva Kharbade <atharvakharbade@gmail.com>
2025-03-27 22:22:43 +0530

    fix: Update directory structure to match Netlify standards

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   distDir: 'public',
   output: 'export'
 }
+
+module.exports = nextConfig
