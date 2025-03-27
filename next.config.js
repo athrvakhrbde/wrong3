f493af65603ee3f2147eb69988c30d3c32cd4386
Atharva Kharbade <atharvakharbade@gmail.com>
2025-03-27 22:12:03 +0530

    fix: Reset to minimal working configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  target: 'serverless',
-  images: {
-    unoptimized: true
-  }
+module.exports = {
+  reactStrictMode: true
 }
-
-module.exports = nextConfig
